1fde2736543d5f2d505a4590bda426cbfae6c582
Krinopotam <omegatester@gmail.com>
2023-10-22 13:08:25 +0300

    dev

--- ui-kit/src/tabulatorBase/tabulatorBase.tsx
+++ ui-kit/src/tabulatorBase/tabulatorBase.tsx
@@ -1,53 +1,63 @@
 import 'tabulator-tables/dist/css/tabulator_simple.css';
-import {ColumnComponent, ColumnDefinition, ColumnLookup, EmptyCallback, EventCallBackMethods, FilterType, RowComponent, TabulatorFull as Tabulator} from 'tabulator-tables';
-import React, {MouseEvent} from 'react';
-import {useInit} from './hooks/init';
 import {
-    IActiveSelectionModuleRow,
-    IActiveSelectionModuleTableEvents,
-    IActiveSelectionTabulator,
-} from './modules/activeSelectionModule';
+    ColumnComponent,
+    ColumnDefinition,
+    ColumnLookup,
+    EmptyCallback,
+    EventCallBackMethods,
+    FilterType,
+    RowComponent,
+    TabulatorFull as Tabulator,
+} from 'tabulator-tables';
+import React, {MouseEvent, useEffect} from 'react';
+import {useInit} from './hooks/init';
+import {IActiveSelectionModuleRow, IActiveSelectionModuleTableEvents, IActiveSelectionTabulator} from './modules/activeSelectionModule';
 import {IAdvancedHeaderFilterTabulator} from './modules/advancedHeaderFilterModule';
-import {HelpersStrings} from "@krinopotam/js-helpers";
-import {Stylization} from "@src/tabulatorBase/stylization";
-import {AnyType} from "@krinopotam/service-types";
+import {HelpersStrings} from '@krinopotam/js-helpers';
+import {Stylization} from '@src/tabulatorBase/stylization';
+import {AnyType} from '@krinopotam/service-types';
 
-type _ITabulator = IAdvancedHeaderFilterTabulator & IActiveSelectionTabulator & Tabulator
+type _ITabulator = IAdvancedHeaderFilterTabulator & IActiveSelectionTabulator & Tabulator;
 
 //region Tabulator types corrections
-export type ITabulatorFilterFunc = (headerValue: AnyType, rowValue: AnyType, rowData: Record<string, unknown>, filterParams: Record<string, unknown>) => boolean;
+export type ITabulatorFilterFunc = (
+    headerValue: AnyType,
+    rowValue: AnyType,
+    rowData: Record<string, unknown>,
+    filterParams: Record<string, unknown>
+) => boolean;
 
 export interface IRequestProps extends Record<string, unknown> {
-    page?: number,
-    size?: number,
-    sort?: { field: string, dir: 'asc' | 'desc' }[],
+    page?: number;
+    size?: number;
+    sort?: {field: string; dir: 'asc' | 'desc'}[];
     filter?: {
-        field: string,
-        type: FilterType | ITabulatorFilterFunc,
-        value: unknown
-    }
+        field: string;
+        type: FilterType | ITabulatorFilterFunc;
+        value: unknown;
+    };
 }
 
 export interface IAjaxConfig extends Record<string, unknown> {
-    method?: 'GET' | 'POST' | 'PUT' | 'DELETE',
+    method?: 'GET' | 'POST' | 'PUT' | 'DELETE';
     /** set request mode to cors */
-    mode?: string,
+    mode?: string;
     /** send cookies with the request from the matching origin */
-    credentials?: string,
+    credentials?: string;
     headers?: Record<string, string> & {
-        "Accept"?: string,
-        "X-Requested-With"?: string,
-        "Content-type"?: string,
-        "Access-Control-Allow-Origin"?: string
-    },
+        Accept?: string;
+        'X-Requested-With'?: string;
+        'Content-type'?: string;
+        'Access-Control-Allow-Origin'?: string;
+    };
 }
 
 export interface ITabulator extends _ITabulator {
     /**  Tabulator property type correction */
-    updateColumnDefinition: (column: ColumnLookup, definition: Partial<ColumnDefinition>) => Promise<void>
+    updateColumnDefinition: (column: ColumnLookup, definition: Partial<ColumnDefinition>) => Promise<void>;
 
     /** Ajax request handler */
-    ajaxRequestFunc?: (url: string, config: IAjaxConfig, params: IRequestProps) => Promise<{ data: Record<string, unknown>[], last_page: number }>;
+    ajaxRequestFunc?: (url: string, config: IAjaxConfig, params: IRequestProps) => Promise<{data: Record<string, unknown>[]; last_page: number}>;
 }
 
 export interface ITabulatorColumn extends Omit<ColumnDefinition, 'headerPopup'> {
@@ -72,7 +82,7 @@ export interface ITabulatorProps extends Omit<ITabulator['options'], 'footerElem
     gridId?: string;
 
     /** Grid columns */
-    columns: ITabulatorColumn[],
+    columns: ITabulatorColumn[];
 
     /** Grid footer element*/
     footerElement?: React.ReactNode;
@@ -92,6 +102,9 @@ export interface ITabulatorProps extends Omit<ITabulator['options'], 'footerElem
     /** Grid container max width*/
     maxWidth?: string | number;
 
+    /** by default user text selection is disabled (CSS user-select='none') */
+    allowTextSelection?: boolean;
+
     //--- The tabulator types correction (The Tabulator has property but has no type for it) ---
     /** If set to true, then when you resize a column its neighbouring column has the opposite resize applied to keep to total width of columns the same */
     resizableColumnFit?: boolean;
@@ -110,12 +123,17 @@ export const TabulatorBase = ({onTableRef, gridId, events, containerClassName, w
 
     useInit({props, events, containerRef, tableRef, onTableRef});
 
-    const containerStyle: React.CSSProperties = {width: width, maxWidth: maxWidth, minWidth: minWidth};
+    const containerStyle: React.CSSProperties = {
+        width: width,
+        maxWidth: maxWidth,
+        minWidth: minWidth,
+        userSelect: !props.allowTextSelection ? 'none' : undefined,
+    };
 
     return (
         <>
-            <Stylization striped={true}/>
-            <div ref={containerRef} id={gridId ?? newId} data-instance={gridId ?? newId} className={containerClassName} style={containerStyle}/>
+            <Stylization striped={true} />
+            <div ref={containerRef} id={gridId ?? newId} data-instance={gridId ?? newId} className={containerClassName} style={containerStyle} />
         </>
     );
 };
