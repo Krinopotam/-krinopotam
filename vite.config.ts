95e3582ea26a83cf07540b1cf783ec4e3ef438ac
Krinopotam <omegatester@gmail.com>
2023-12-04 16:00:35 +0300

    Updates

---
+++ vite.config.ts
@@ -0,0 +1,48 @@
+import {defineConfig, UserConfig} from 'vite'
+import * as path from "path";
+import react from '@vitejs/plugin-react-swc'
+import {watch} from "vite-plugin-watch"
+
+const commonCfg: UserConfig = {
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@src": path.resolve(__dirname, "./src"),
+        },
+    },
+    build: {
+        target: 'modules',
+        emptyOutDir:true,
+        //minify: 'terser',
+    },
+}
+
+const devDemoCfg: UserConfig = {
+    ...commonCfg,
+    plugins: [
+        ...commonCfg.plugins,
+        watch({
+            pattern: ['demo/components/**/*.*'],
+            command: "node ./demo/.generator/generateDemo.js",
+        }),
+    ],
+    server: {
+        open: 'index.html',
+    }
+}
+
+const buildDemoCfg: UserConfig = {
+    ...commonCfg,
+    base: '/-krinopotam/ui-kit', //base URL (gitHub pages use https://krinopotam.github.io/-krinopotam/ui-kit, so URL is https://krinopotam.github.io and baseURL is /-krinopotam/ui-kit)
+    build: {
+        ...commonCfg.build,
+        outDir:'demo-build/ui-kit',
+    }
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default defineConfig(({command, mode}) => {
+    console.log('----------------',command, mode)
+    if (mode === 'demo') return command === 'serve' ? devDemoCfg : buildDemoCfg
+    return commonCfg
+})
