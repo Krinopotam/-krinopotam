65ad120856303c2820b3bae488f300f29a84a89e
Krinopotam <omegatester@gmail.com>
2024-08-26 18:03:18 +0300

    dev

--- ui-kit/src/waiter/waiter.tsx
+++ ui-kit/src/waiter/waiter.tsx
@@ -1,5 +1,5 @@
 import React, {PropsWithChildren} from 'react';
-import {Spin} from 'antd';
+import {Spin, theme} from 'antd';
 
 export interface IWaiterProps {
     show: boolean;
@@ -10,6 +10,12 @@ export interface IWaiterProps {
 }
 
 export const Waiter = (props: PropsWithChildren<IWaiterProps>) => {
+    const {
+        token: {colorBgBase},
+    } = theme.useToken();
+
+    const isDarkMode = colorBgBase === '#000';
+
     const style: React.CSSProperties = {
         position: props.fullScreen ? 'fixed' : 'absolute',
         top: 0,
@@ -21,14 +27,14 @@ export const Waiter = (props: PropsWithChildren<IWaiterProps>) => {
         justifyContent: 'center',
         zIndex: 2000,
         transition: 'all 0.2s',
-        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        backgroundColor: isDarkMode ? 'rgba(0, 0, 0, 0.5)' : 'rgba(255, 255, 255, 0.5)',
         ...props.style
     };
 
     return (
         <>
             {props.show && <div style={style}>{props.showSpinner !== false && <Spin/>}</div>}
-            {props.showChildren && props.children}
+            {props.show ? props.showChildren && props.children : props.children}
         </>
     );
 };
