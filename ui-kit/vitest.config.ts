083db80667ed7e84e4fb7cf04ffc672dcb91f4e8
Krinopotam <omegatester@gmail.com>
2024-09-27 23:15:44 +0300

    dev

--- ui-kit/vitest.config.ts
+++ ui-kit/vitest.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig, mergeConfig} from 'vitest/config';
 import viteConfig from './vite.config';
-import path from 'path';
 
 export default defineConfig(configEnv =>
     mergeConfig(
