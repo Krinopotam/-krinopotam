5a89741f61a1759a7d63f2fd8d8a8e3464b5fc57
Krinopotam <omegatester@gmail.com>
2025-03-21 15:48:41 +0300

    Updates

---
+++ ui-kit/storybook/assets/tabulatorGrid.stories-DNdQASNY.js
@@ -0,0 +1,72 @@
+import{j as A}from"./jsx-runtime-D_zvdyIk.js";import{a as _,b as C}from"./tabulator-BA42q_kp.js";import{T as R}from"./tabulatorGrid-DqXu-7ge.js";import{f as G}from"./index-cNkFZXRj.js";import"./index-iW-139IE.js";import"./client-DLm75T6k.js";import"./index-D_2t0AaZ.js";import"./isArray-Cl7xn3Rc.js";import"./isDebugMode-B-VM_G4X.js";import"./getNanoId-CghKzzgc.js";import"./styled-components.browser.esm-Di54_BhW.js";import"./index-CMYZVoVF.js";import"./useToken-C9qwXZKU.js";import"./dayjs.min-B6ICIYLQ.js";import"./dForm-CaK-ZZvS.js";import"./buttonsRow-r31VO2nI.js";import"./button-DSzQwbIX.js";import"./compact-item-Cxv0YLF9.js";import"./index-mQZ8i_rL.js";import"./button-D87aS1YS.js";import"./Sider-zAVKy6og.js";import"./index-H-U0Qhl4.js";import"./context-u9dygr8g.js";import"./KeyCode-lh1qUinJ.js";import"./row-Bq2vx5ix.js";import"./ContextIsolator-BX2WkAUK.js";import"./motion-HGHEQR47.js";import"./roundedArrow-CIJYWJcZ.js";import"./index-Bi_XhfGV.js";import"./colors-B0jgcrR7.js";import"./DownOutlined-CSg1yAI9.js";import"./updateModalTheme-DsqyeQsT.js";import"./headerRender-jf3Za5Ns.js";import"./index-CvIgV7lr.js";import"./ExclamationCircleFilled-DO6fsheT.js";import"./pickAttrs-DOtYDQG6.js";import"./CloseOutlined-C5nLa7GN.js";import"./useClosable-DLVmVXm2.js";import"./InfoCircleOutlined-DgVG6Ryw.js";import"./QuestionCircleOutlined-BUGNUdLn.js";import"./index-DSj8ZEWj.js";import"./TextArea-Bx-nz3Fu.js";import"./useVariants-Cpy17TlJ.js";import"./index-CHrs_Nvn.js";import"./CheckOutlined-DXdfq3FB.js";import"./dFormModal-DEStx9I4.js";import"./modal-CWXUBKzX.js";import"./splitObject-C6hVKqbV.js";import"./defaultButtonsProps-DpkFXSYy.js";import"./List-mPb_Fs88.js";import"./index-BspaYdSE.js";import"./v4-CtRu48qb.js";import"./EyeOutlined-BdSgb3nL.js";import"./DeleteOutlined-S5uC3Dcu.js";import"./iframe-Bl7ekN0r.js";import"./MenuOutlined-uwesescX.js";const Le={title:"Controls/TabulatorGrid",component:R,tags:["autodocs"],parameters:{docs:{}}},t={dataSet:C,columns:_,columnDefaults:{resizable:"header",headerFilter:!0,headerFilterFunc:"like"},height:500,layout:"fitColumns",movableColumns:!0,onDataFetch:void 0,onDataFetchResponse:void 0,onMenuVisibilityChanged:G()},r={args:{...t}},o={args:{...t,showHeaderFilter:!0,footerElement:A.jsx("div",{children:"My Footer"})}},a={args:{...t,dataTree:!0}},i={args:{...t,footerElement:A.jsx("div",{children:"My Footer"})}},n={args:{...t,height:void 0,pagination:!0,paginationMode:"local",paginationSize:10,paginationSizeSelector:[5,10,50,100],paginationCounter:"rows"}},s={args:{...t,dataSet:void 0,height:void 0,pagination:!0,paginationSize:5,paginationSizeSelector:[5,10,50,100],paginationCounter:"rows",paginationMode:"remote",filterMode:"remote",sortMode:"remote",onDataFetch:(e,k)=>new Promise(D=>{setTimeout(()=>{const p=(e==null?void 0:e.page)??1,m=(e==null?void 0:e.size)??k.getProps().paginationSize??5,j=4,E=C.slice((p-1)*m,p*m);D({data:E,last_page:j})},1e3)}),ajaxParams:{key1:"value1",key2:"value2"}}};var c,d,g;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    ...baseArgs
+  }
+}`,...(g=(d=r.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var l,u,h;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    ...baseArgs,
+    showHeaderFilter: true,
+    footerElement: <div>My Footer</div>
+  }
+}`,...(h=(u=o.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};var S,f,v;a.parameters={...a.parameters,docs:{...(S=a.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    ...baseArgs,
+    dataTree: true
+  }
+}`,...(v=(f=a.parameters)==null?void 0:f.docs)==null?void 0:v.source}}};var b,z,F;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    ...baseArgs,
+    footerElement: <div>My Footer</div>
+  }
+}`,...(F=(z=i.parameters)==null?void 0:z.docs)==null?void 0:F.source}}};var T,y,P;n.parameters={...n.parameters,docs:{...(T=n.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    ...baseArgs,
+    height: undefined,
+    pagination: true,
+    paginationMode: 'local',
+    paginationSize: 10,
+    paginationSizeSelector: [5, 10, 50, 100],
+    paginationCounter: 'rows'
+  }
+}`,...(P=(y=n.parameters)==null?void 0:y.docs)==null?void 0:P.source}}};var M,w,x;s.parameters={...s.parameters,docs:{...(M=s.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    ...baseArgs,
+    dataSet: undefined,
+    height: undefined,
+    pagination: true,
+    paginationSize: 5,
+    paginationSizeSelector: [5, 10, 50, 100],
+    paginationCounter: 'rows',
+    paginationMode: 'remote',
+    filterMode: 'remote',
+    sortMode: 'remote',
+    onDataFetch: (params, gridApi) => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          //make any remote fetch
+          const page = params?.page ?? 1;
+          const size = params?.size ?? gridApi.getProps().paginationSize ?? 5;
+          const lastPage = 4;
+          const dataSet = TabulatorTreeDataset.slice((page - 1) * size, page * size); //remote fetch imitation
+          resolve({
+            data: dataSet,
+            last_page: lastPage
+          });
+        }, 1000);
+      });
+    },
+    /*        ajaxRequestFunc: (url, config, params) => {\r
+        return new Promise((resolve) => {\r
+            setTimeout(() => {\r
+                //make any remote fetch\r
+                const dataSet = TabulatorTreeDataset.slice((params.page - 1) * params.size, params.page * params.size); //remote fetch imitation\r
+                resolve({data: dataSet, last_page: 4})\r
+            }, 1000)\r
+        })\r
+    },*/
+    ajaxParams: {
+      key1: 'value1',
+      key2: 'value2'
+    } //any additional params (you can pass a callback)
+  }
+}`,...(x=(w=s.parameters)==null?void 0:w.docs)==null?void 0:x.source}}};const qe=["Simple","WithHeaderFilter","Tree","WithFooter","PaginationLocal","PaginationRemote"];export{n as PaginationLocal,s as PaginationRemote,r as Simple,a as Tree,i as WithFooter,o as WithHeaderFilter,qe as __namedExportsOrder,Le as default};
